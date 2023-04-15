3f296919f4b26e6f397e5014b3710a4f1de53aec
Thao.Tran <thaotran.k39ueh.qb@gmail.com>
2023-04-16 00:04:45 +0700

    update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1><b>Hello World!</b></h1>
+      <h1> I'm Sia Alita</h1>
     </main>
   )
 }
